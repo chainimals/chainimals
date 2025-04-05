88c54f39d64f875b37a23832de18b522a2f65387
April Yang <73070002+yang4tech@users.noreply.github.com>
2025-04-05 23:16:58 +0800

    update index.js for IPFS CID

--- src/config/index.js
+++ src/config/index.js
@@ -13,5 +13,19 @@ export const networkConfig = {
 };
 
 // 智能合約地址和ABI
-export const contractAddress = "YOUR_CONTRACT_ADDRESS_HERE";
-export const contractABI = []; // 放入你的合約ABI
+export const contractAddress = "0xc5a7c88Cf583bCA01349Df7e47520F3D8AE7c0D0";
+export const contractABI = [
+  // 填入你的合約 ABI
+  "function mintNFT(string memory tokenURI) public returns (uint256)",
+  // ... 其他函數
+];
+
+
+// 添加到現有的 config 文件中
+export const IPFS_MAP = {
+  'D': 'ipfs://bafkreigk5mgiax3o43clpgnhmo6pn2i5kkmgxpzfdyan7tix42fvl4xbdu', // 更換為實際的 IPFS CID
+  'I': 'ipfs://bafkreigk5mgiax3o43clpgnhmo6pn2i5kkmgxpzfdyan7tix42fvl4xbdu',
+  'S': 'ipfs://bafkreigk5mgiax3o43clpgnhmo6pn2i5kkmgxpzfdyan7tix42fvl4xbdu',
+  'C': 'ipfs://bafkreigk5mgiax3o43clpgnhmo6pn2i5kkmgxpzfdyan7tix42fvl4xbdu',
+  'balanced': 'ipfs://bafkreigk5mgiax3o43clpgnhmo6pn2i5kkmgxpzfdyan7tix42fvl4xbdu'
+};
